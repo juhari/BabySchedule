506cc2b6e44533832920a76a1b7d8aee3bb1f593
Juha Riippi <juhari@pekka-virtanen.local>
2012-05-05 10:38:21 +0300

    Initial iOS project

---
+++ iOS/BabySchedule/BabySchedule/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BabySchedule
+//
+//  Created by Juha Riippi on 5.5.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/BabySchedule/BabyScheduleTests/BabyScheduleTests.h
@@ -0,0 +1,13 @@
+//
+//  BabyScheduleTests.h
+//  BabyScheduleTests
+//
+//  Created by Juha Riippi on 5.5.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BabyScheduleTests : SenTestCase
+
+@end
